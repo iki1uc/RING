4b0ca9f73e1110e71be19503e8aed16e124436c3
iki1uc <entertrainer@web.de>
2026-08-26 17:34:59 +0200

    Update sli.continuum.js

--- sli/continuum/sli.continuum.js
+++ sli/continuum/sli.continuum.js
@@ -1,1 +1,10 @@
+export function SLI_CONTINUUM(pipe, matrix, tmp) {
+    return {
+        pipe,
+        matrix,
+        tmp,
+        layer: "CONTINUUM",
+        view: "SLI-CONTINUUM"
+    };
+}
 
