c27d8450a4f3e3b1c42bd4b7463ecb07dab44ff5
iki1uc <entertrainer@web.de>
2026-08-26 17:18:34 +0200

    Update NC.js

--- NC/NC.js
+++ NC/NC.js
@@ -1,20 +1,15 @@
 // NC/nc.js
-import { CORE } from "../CORE/core.js";
-import { PIPE } from "../PIPE/pipe.js";
-import { SLI } from "../SLI/sli.js";
+import { NC_WORK } from "./nc.work.js";
+import { NC_SYNC } from "./nc.sync.js";
+import { NC_3X3 } from "./nc.3x3.js";
 
-export function NC(run = 1, mode = "seq") {
-
-    const core = CORE(run);
-    const pipe = PIPE(run, mode);
-    const sli = SLI(run, pipe);
+export function NC(stack = [], core, sli) {
 
     return {
-        run,
-        mode,
-        core,
-        pipe,
-        sli,
+        stack,
+        work: NC_WORK(stack),
+        sync: NC_SYNC(core, sli),
+        matrix: NC_3X3(),
         auto: true
     };
 }
