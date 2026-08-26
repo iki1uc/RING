849a13e1e5eb67cfef3e2427eb819b4dd439acd4
iki1uc <entertrainer@web.de>
2026-08-26 17:44:52 +0200

    Create mind.icefeuer.js

---
+++ core/mind.icefeuer.js
@@ -0,0 +1,15 @@
+// CORE/mind.icefeuer.js
+
+export function ICEFEUER(dusche, sonne, albtraum) {
+
+    return {
+        wasser: dusche,
+        feuer: sonne,
+        schatten: albtraum,
+        axiomA: "WASSER-A",
+        axiomB: "FEUER-B",
+        axiomC: "SCHATTEN-C",
+        qsyn: "QUNDµSYN",
+        state: "ICE-FEUER-MIND"
+    };
+}
