c8ee642d1d8b24c0a2d4263bc43c31ed645e12fc
iki1uc <entertrainer@web.de>
2026-08-26 17:09:23 +0200

    Update line.js

--- pipe/line.js
+++ pipe/line.js
@@ -1,1 +1,22 @@
+// line.js
+import { AXIOM3 } from "./axiom3.js";
+
+export function LINE(mode = "seq") {
+
+    if (mode === "seq") {
+        return [...AXIOM3.ONE, ...AXIOM3.TWO, ...AXIOM3.THREE];
+    }
+
+    if (mode === "mix") {
+        return [
+            AXIOM3.ONE[0], AXIOM3.TWO[0], AXIOM3.THREE[0],
+            AXIOM3.ONE[1], AXIOM3.TWO[1], AXIOM3.THREE[1],
+            AXIOM3.ONE[2], AXIOM3.TWO[2], AXIOM3.THREE[2]
+        ];
+    }
+
+    if (mode === "blitz") {
+        return AXIOM3;
+    }
+}
 
