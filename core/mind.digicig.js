813d0ce7680eb958ab0766f4538b7536a5dd4434
iki1uc <entertrainer@web.de>
2026-08-26 17:42:40 +0200

    Create mind.digicig.js

---
+++ core/mind.digicig.js
@@ -0,0 +1,12 @@
+// CORE/mind.digicig.js
+
+export function DIGI_CIG(state = "flow") {
+    return {
+        state,
+        impulse: "WARM-FLUST",
+        kick: "ESPRESSO",
+        art: "PICASSO",
+        layer: "AXIOMC",
+        bonus: "ATLANTIS"
+    };
+}
