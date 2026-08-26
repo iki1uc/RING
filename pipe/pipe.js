6ed494028325c9645a8a1db247acc2ba4a0b428d
iki1uc <entertrainer@web.de>
2026-08-26 17:09:11 +0200

    Update pipe.js

--- pipe/pipe.js
+++ pipe/pipe.js
@@ -1,1 +1,21 @@
+// pipe.js
+import { LINE } from "./line.js";
+import { AXIOM3 } from "./axiom3.js";
+
+export function PIPE(run = 1, mode = "seq") {
+
+    const out = {
+        run,
+        mode,
+        line: LINE(mode),
+        axiom: AXIOM3,
+        bonus: null
+    };
+
+    if (run === 3) {
+        out.bonus = "ICEFEUER";
+    }
+
+    return out;
+}
 
